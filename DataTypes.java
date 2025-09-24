29702a2f32c56b3f47729037ea9cc46d16ed3063
vidyaan <vidyav340995@gmail.com>
2025-09-24 12:25:24 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int age = 24;
+        long phone = 8562412456L;
+
+        float height = 85.35f;
+        double pi = 3.142125;
+
+        char section = 'A';
+        String name = "Akshay Rao";
+        boolean isTrainer = true;
+
+
+        System.out.println("Name (String): " + name);
+        System.out.println("Favourite Constant (Pi - Double Float): " + pi);
+
+        System.out.println("Age (Integer): " + age);
+        System.out.println("Height (Float): " + height);
+        System.out.println("Section (Character): " + section);
+        System.out.println("Are you a Trainer? (Boolean): " + isTrainer);
+        System.out.println("Phone Number (Long Integer): " + phone);
+
+    }
+}
